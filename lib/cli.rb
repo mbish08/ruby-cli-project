1072330f8b5fe7a041bec4ac92d6415e284293fc
mbish08 <mdrew8@gmail.com>
2020-10-03 11:51:29 -0700

    backed up to reassess

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,6 +6,11 @@ class CLI
     def start 
         #start with greeting
         puts ""
+        puts ".~~~~."
+        puts "i====i_"
+        puts "|cccc|_)"
+        puts "|cccc|"   
+        puts "`-==-"
         puts "Welcome to Brewery Finder!"
         #ask user to indicate if they would like to search by city or state
         puts ""
@@ -14,12 +19,25 @@ class CLI
         @city = gets.strip.downcase 
         puts ""
         API.fetch_cities(@city)
-        Breweries.all 
+        breweries = Breweries.all 
+        print_all(breweries)
+        puts ""
+        puts "If you would like to see more info, type the number of the brewery."
+        puts "Type 'exit' to exit the search."
+        puts ""
+        selection = gets.strip.downcase 
         binding.pry 
 
 
 
     end 
  
+    def print_all(brewery) #this method creates the numbered list that the user will see and includes the city/state
+        puts "Here are the breweries for city names that match #{@city.capitalize}."
+        puts ""
+        brewery.each_with_index do | b, i |
+            puts "#{i+1}. #{b.name}: #{b.city}, #{b.state}"
+        end 
+    end 
  
 end
