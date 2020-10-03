a998634c396eb48da28b70b36aca10234b80728f
mbish08 <mdrew8@gmail.com>
2020-10-03 15:59:46 -0700

    city selection working

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,8 @@
 #contains all of the gets and puts
  
 class CLI 
+
+    attr_accessor :selection 
  
     def start 
         #start with greeting
@@ -25,18 +27,16 @@ class CLI
         puts "If you would like to see more info, type the number of the brewery."
         puts "Type 'exit' to exit the search."
         puts ""
-        select = gets.strip.downcase.to_i - 1
-        while select != "exit" do 
+        @selection = gets.strip.downcase.to_i - 1
+        if self.selection != "exit" 
             more_info(breweries) 
-
-         
 
         end 
 
     end 
  
     def print_all(brewery) #this method creates the numbered list that the user will see and includes the city/state
-        puts "Here are the breweries for city names that match #{@city.capitalize}."
+        puts "Here are the breweries for city names that match #{@city.capitalize}:"
         puts ""
         brewery.each_with_index do | b, i |
             puts "#{i+1}. #{b.name}: #{b.city}, #{b.state}"
@@ -45,9 +45,16 @@ class CLI
  
     def more_info(brewery)
         puts "Here is the information for your selection:"
-        binding.pry 
-
+        puts "Name: #{brewery[selection].name}"
+        puts "Address: #{brewery[selection].street}"
+        puts "#{brewery[selection].city}, #{brewery[selection].state}, #{brewery[selection].postal_code}"
+        puts "Phone: #{brewery[selection].phone}"
+        puts "Website: #{brewery[selection].website_url}"
         
+       # binding.pry 
+        # brewery[1].name will equal the name
+
+
     end 
  
 end
